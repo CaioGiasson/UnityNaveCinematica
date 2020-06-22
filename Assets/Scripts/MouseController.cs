934ae45c0eb2288333d38b2cadb18574544ad304
CaioGiasson <cainitech@gmail.com>
2020-06-21 23:24:29 -0300

    Implementados 3 exercicios

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -1,32 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MouseController : MonoBehaviour {
 
-    public static MouseController controller;
-	private void Awake() { controller = this; }
+	public static MouseController controller;
+	private void Awake ( ) { controller = this; }
 
-    public GameObject theX;
+	public GameObject theX;
+	public Text texto;
 
-    void Update(){
+	public int qualExercicio;
 
-		if ( Input.GetMouseButtonUp(0)){
-            Vector3 pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
-            Vector3 posConvert = Camera.main.ScreenToWorldPoint(pos);
-            posConvert.z = 10;
-            GameObject x = Instantiate(theX);
-            x.transform.position = posConvert;
-            StartCoroutine(RemoverX(x));
+	public void setExercicio ( int qual ) {
+		qualExercicio = qual;
+	}
 
-            NaveController.nave.IrPara(posConvert);
+	void Update ( ) {
+
+		switch ( qualExercicio ) {
+			case 1:
+				texto.text = "Exercício de Cinemática\n\nAo clicar em um lugar qualquer da tela,\na nave gira para essa direção em 1 segundo\ne depois se desloca até o local no decorrer de 5 segundos.";
+				break;
+			case 2:
+				texto.text = "Exercício de Quantidade de Movimento\n\nA nave sempre \"olha\" na direção do ponteiro do mouse.\nQuando o usuário clica, os propulsores são ligados.\nIsso gera um impulso I = F * t \nSendo F a força de propulsão e t a duração do impulso.\nComo o impulso é igual à variação na quantidade de movimento:\nI = Qf - Q0\nI = delta Q\nI = m * delta V\nAssim, a variação da velocidade vale I / m\nVariação da velocidade = F * t / m";
+				break;
+			case 3:
+			default:
+				texto.text = "Força resultante = Fgravidade + Fmotores\nAceleração = Força resultante / massa\nVariaçào da velocidade = aceleração * intervalo de tempo (deltaTime)\nA cada frame a velocidade recebe um incremento segundo esses parâmetros.\n\nO chão possui restituição total, portanto,\na nave não perde velocidade ao colidir com o chão.\nPor conta disso ela \"rebate\", voltando \na se mover para cima com a mesma velocidade de queda\n imediatamente anterior.";
+				break;
 		}
 
-    }
+		// CÓDIGO ABAIXO É PARA O EXERCÍCIO 1
+		if ( qualExercicio == 1 ) {
+			if ( Input.GetMouseButtonUp ( 0 ) ) {
+				Vector3 pos = new Vector3 ( Input.mousePosition.x, Input.mousePosition.y, 0 );
+				Vector3 posConvert = Camera.main.ScreenToWorldPoint ( pos );
+				posConvert.z = 10;
+				GameObject x = Instantiate ( theX );
+				x.transform.position = posConvert;
+				StartCoroutine ( RemoverX ( x ) );
 
-	IEnumerator RemoverX(GameObject g){
-        yield return new WaitForSeconds(3.0f);
-        Destroy(g.gameObject);
+				NaveController.nave.IrPara ( posConvert );
+			}
+		}
+		else
+		//CÓDIGO ABAIXO É PARA O EXERCÍCIO 2
+		if ( qualExercicio == 2 ) {
+			Vector3 pos = new Vector3 ( Input.mousePosition.x, Input.mousePosition.y, 0 );
+			Vector3 posConvert = Camera.main.ScreenToWorldPoint ( pos );
+
+			NaveController.nave.Apontar ( posConvert );
+
+			if ( Input.GetMouseButton ( 0 ) ) NaveController.nave.LigarJatos ( );
+			else NaveController.nave.DesligarJatos ( );
+		}
+		else
+		// CÓDIGO ABAIXO É PARA O EXERCÍCIO 3. ESTÁ IGUAL O DO 2 MAS SEPARADO PARA INDICAR QUE
+		// SE NECESSÁRIO PODEM SER IMPLEMENTADOS SEPARADAMENTE.
+		if ( qualExercicio == 3 ) {
+			Vector3 pos = new Vector3 ( Input.mousePosition.x, Input.mousePosition.y, 0 );
+			Vector3 posConvert = Camera.main.ScreenToWorldPoint ( pos );
+
+			NaveController.nave.Apontar ( posConvert );
+
+			if ( Input.GetMouseButton ( 0 ) ) NaveController.nave.LigarJatos ( );
+			else NaveController.nave.DesligarJatos ( );
+		}
+
+		// CÓDIGO PARA O RELOAD AO TECLAR "R"
+		if ( Input.GetKeyUp ( KeyCode.R ) ) {
+			SceneManager.LoadScene ( SceneManager.GetActiveScene ( ).buildIndex );
+		}
+	}
+
+	IEnumerator RemoverX ( GameObject g ) {
+		yield return new WaitForSeconds ( 3.0f );
+		Destroy ( g.gameObject );
 	}
 
 }
--- Assets/Scripts/NaveController.cs
+++ Assets/Scripts/NaveController.cs
@@ -2,50 +2,170 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NaveController : MonoBehaviour
-{
+public class NaveController : MonoBehaviour {
 
 	public static NaveController nave;
-	private void Awake() { nave = this; }
+	private void Awake ( ) { nave = this; }
 
 	public float tempoRotacao = 10.0f, tempoTranslacao = 5.0f;
 
+	// DADOS DO EXERCICIO 1
 	private Vector3 alvo;
 	float distanciaAngular, deslocamentoAngular, passoAngular;
 	Vector3 distanciaLinear, deslocamentoLinear, passoLinear;
 
-	private void Update(){
-		
+	// DADOS DO EXERCICIO 2
+	int e;
+	public float massaNave, forcaJato;
+	private float Vx, Vy, Fx, Fy;
+	private Vector3 direcao;
+	public ParticleSystem jato;
+	private Rigidbody rb;
+
+	// DADOS DO EXERCICIO 3
+	public float gravidade;
+	public GameObject chao;
+
+	// DADOS PARA FAZER O WARP DA TELA QUANDO CHEGA NA BORDA
+	float zFixo = 10.0f;
+	Camera c;
+
+	private void Start ( ) {
+		rb = GetComponent<Rigidbody> ( );
+		e = MouseController.controller.qualExercicio;
+		if ( e == 1 ) {
+			rb.useGravity = false;
+			rb.isKinematic = false;
+
+			chao.SetActive ( false );
+
+		}
+		else if ( e == 2 ) {
+			rb.useGravity = false;
+			rb.isKinematic = false;
+
+			chao.SetActive ( false );
+		}
+		else if ( e == 3 ) {
+			// DEIXEI A GRAVIDADE NATIVA COMO FALSE PARA PODER USAR A GRAVIDADE ARITMÉTICA, APLICADA DIRETO COMO FORÇA (NA fixedUpdate)
+			rb.useGravity = false;
+			rb.isKinematic = false;
+
+			chao.SetActive ( true );
+		}
+
 	}
 
-	public void IrPara(Vector3 novoAlvo) {
+	public void Apontar ( Vector3 direcaoApontar ) {
+		direcao = direcaoApontar;
+	}
+
+	public void LigarJatos ( ) {
+		if ( jato.isStopped ) jato.Play ( );
+	}
+
+	public void DesligarJatos ( ) {
+		if ( jato.isPlaying ) jato.Stop ( );
+	}
+
+	public void IrPara ( Vector3 novoAlvo ) {
 		alvo = novoAlvo;
 
-		distanciaLinear = Vet.Subtrai(alvo, transform.position);
+		distanciaLinear = Vet.Subtrai ( alvo, transform.position );
 
-		float anguloNovo = Mathf.Atan2(distanciaLinear.y, distanciaLinear.x) * Mathf.Rad2Deg + 90;      // ESSE +90 É POR QUE A ORIGEM FICA PRA CIMA E NÃO PARA A DIREITA COMO NO CÍRCULO TRIGONOMÉTRICO PADRÃO
+		float anguloNovo = Mathf.Atan2 ( distanciaLinear.y, distanciaLinear.x ) * Mathf.Rad2Deg + 90;      // ESSE +90 É POR QUE A ORIGEM FICA PRA CIMA E NÃO PARA A DIREITA COMO NO CÍRCULO TRIGONOMÉTRICO PADRÃO
 		float anguloAtual = transform.rotation.eulerAngles.z;
 		distanciaAngular = anguloNovo - anguloAtual;
-		if (distanciaAngular > 360.0f) distanciaAngular -= 360.0f;
-		if (distanciaAngular < -360.0f) distanciaAngular += 360.0f;
-		deslocamentoAngular = Mathf.Abs(distanciaAngular);
+		if ( distanciaAngular > 360.0f ) distanciaAngular -= 360.0f;
+		if ( distanciaAngular < -360.0f ) distanciaAngular += 360.0f;
+		deslocamentoAngular = Mathf.Abs ( distanciaAngular );
 
 		deslocamentoLinear = distanciaLinear;
 	}
 
-	private void FixedUpdate()
-	{
-		if ( deslocamentoAngular>1.0f )
-		{
-			passoAngular = distanciaAngular * Time.deltaTime / tempoRotacao;
-			transform.rotation = Quaternion.Euler( Vet.Soma(transform.rotation.eulerAngles, new Vector3(0, 0, passoAngular )));
-			deslocamentoAngular -= Mathf.Abs(passoAngular);
+	private void FixedUpdate ( ) {
+		e = MouseController.controller.qualExercicio;
+
+		if ( e == 1 ) {
+			chao.SetActive ( false );
+
+			if ( deslocamentoAngular > 1.0f ) {
+				passoAngular = distanciaAngular * Time.deltaTime / tempoRotacao;
+				transform.rotation = Quaternion.Euler ( Vet.Soma ( transform.rotation.eulerAngles, new Vector3 ( 0, 0, passoAngular ) ) );
+				deslocamentoAngular -= Mathf.Abs ( passoAngular );
+			}
+			else if ( Vet.Magnitude ( deslocamentoLinear ) > 1.0f ) {
+				passoLinear = distanciaLinear * Time.deltaTime / tempoTranslacao;
+				transform.position = Vet.Soma ( transform.position, passoLinear );
+				deslocamentoLinear = Vet.Subtrai ( deslocamentoLinear, passoLinear );
+			}
 		}
-		else if ( Vet.Magnitude(deslocamentoLinear)>1.0f )
-		{
-			passoLinear = distanciaLinear * Time.deltaTime / tempoTranslacao;
-			transform.position = Vet.Soma(transform.position, passoLinear);
-			deslocamentoLinear = Vet.Subtrai(deslocamentoLinear, passoLinear);
+		else if ( e == 2 ) {
+
+			chao.SetActive ( false );
+
+			if ( direcao != null ) {
+				Vector3 diferencaAngular = Vet.Subtrai ( direcao, transform.position );
+				float anguloNovo = Mathf.Atan2 ( diferencaAngular.y, diferencaAngular.x ) * Mathf.Rad2Deg + 90;
+				transform.rotation = Quaternion.Euler ( new Vector3 ( 0, 0, anguloNovo ) );
+
+				if ( !jato.isStopped ) {
+					// ALTERANDO A QUANTIDADE DE MOVIMENTO / MOMENTO LINEAR
+					// MASSA É CONSTANTE, PORTANTO, O QUE VARIA É A VELOCIDADE
+					float impulso = forcaJato * Time.fixedDeltaTime;
+					// IMPULSO É IGUAL À VARIAÇÃO DA QUANTIDADE DE MOVIMENTO
+					float deltaQ = impulso;
+					float deltaV = deltaQ / massaNave;
+					float deltaVx = deltaV * Mathf.Cos ( ( transform.root.eulerAngles.z - 90 ) * Mathf.Deg2Rad );
+					float deltaVy = deltaV * Mathf.Sin ( ( transform.root.eulerAngles.z - 90 ) * Mathf.Deg2Rad );
+					Vx += deltaVx;
+					Vy += deltaVy;
+				}
+
+				Vector3 tp = transform.position;
+				transform.position = new Vector3 (
+					tp.x + Vx * Time.fixedDeltaTime,
+					tp.y + Vy * Time.fixedDeltaTime,
+					zFixo
+				);
+			}
+		}
+		else if ( e == 3 ) {
+
+			chao.SetActive ( true );
+
+			if ( direcao != null ) {
+				Vector3 diferencaAngular = Vet.Subtrai ( direcao, transform.position );
+				float anguloNovo = Mathf.Atan2 ( diferencaAngular.y, diferencaAngular.x ) * Mathf.Rad2Deg + 90;
+				transform.rotation = Quaternion.Euler ( new Vector3 ( 0, 0, anguloNovo ) );
+
+				float forcaX = 0;
+				float forcaY = gravidade;
+
+				if ( !jato.isStopped ) {
+					// AGORA SEPARANDO JÁ NO IMPULSO OS VALORES EM X E Y
+					// POR QUE GRAVIDADE SÓ VAI AFETAR EM Y
+					forcaX += forcaJato * Mathf.Cos ( ( transform.root.eulerAngles.z - 90 ) * Mathf.Deg2Rad );
+					forcaY += forcaJato * Mathf.Sin ( ( transform.root.eulerAngles.z - 90 ) * Mathf.Deg2Rad );
+				}
+
+				float Ax = forcaX / massaNave;
+				float Ay = forcaY / massaNave;
+
+				Vx = Vx + Ax * Time.fixedDeltaTime;
+				Vy = Vy + Ay * Time.fixedDeltaTime;
+
+				Vector3 tp = transform.position;
+				transform.position = new Vector3 (
+					tp.x + Vx * Time.fixedDeltaTime,
+					tp.y + Vy * Time.fixedDeltaTime,
+					zFixo
+				);
+
+				if ( transform.position.y < -4.2 ) {
+					Vy *= -1;
+				}
+			}
 		}
 
 	}
